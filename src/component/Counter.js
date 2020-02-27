ae37815895c35966e6d54580b0319e65894e52d2
Eric Atento <eric.atento@boom.camp>
2020-02-27 17:28:04 +0800

    Update

--- src/component/Counter.js
+++ src/component/Counter.js
@@ -7,33 +7,35 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Button from "@material-ui/core/Button";
 const Data = {
-  message: "",
-  messages: []
+  firstName: "",
+  lastName: "",
+  fullNames: []
 };
 
 export default function Counter() {
   const classes = useStyles();
   const [state, dispatch] = useReducer(reducer, Data);
+  const { firstName, lastName, fullNames } = state;
   const dense = false;
 
   const handleChange = e => {
     dispatch({
-      type: "message",
-      payload: e.target.value
-    });
-  };
-  const handleSubmit = e => {
-    e.preventDefault();
-    dispatch({
-      type: "getMessages",
-      payload: [...state.messages, state.message]
-    });
-    dispatch({
-      type: "message",
-      payload: ""
+      type: "form",
+      field: e.target.name,
+      value: e.target.value
     });
   };
 
+  const handleSubmit = e => {
+    console.log(e);
+    e.preventDefault();
+    dispatch({
+      type: "getName",
+      fullName: [...fullNames, firstName + " " + lastName]
+    });
+  };
+
+  console.log(fullNames);
   return (
     <>
       <form
@@ -46,14 +48,25 @@ export default function Counter() {
           id="outlined-basic"
           label="Type Text"
           variant="outlined"
+          name="firstName"
           onChange={handleChange}
-          value={state.message}
+          value={firstName}
         />
-        <Button type="submit">Submit</Button>
+        <TextField
+          id="outlined-basic"
+          label="Type Text"
+          variant="outlined"
+          name="lastName"
+          onChange={handleChange}
+          value={lastName}
+        />
+        <Button variant="contained" type="submit">
+          Submit
+        </Button>
       </form>
       <div className={classes.demo}>
         <List dense={dense}>
-          {state.messages.map((data, i) => (
+          {fullNames.map((data, i) => (
             <ListItem key={i}>
               <ListItemText primary={data} />
             </ListItem>
--- src/component/reducer.js
+++ src/component/reducer.js
@@ -1,11 +1,16 @@
 export default function reducer(state, action) {
   // console.log(state);
-  // console.log(action);
+  console.log(action);
   switch (action.type) {
-    case "message":
-      return { ...state, message: action.payload };
-    case "getMessages":
-      return { ...state, messages: action.payload };
+    case "form":
+      return { ...state, [action.field]: action.value };
+    case "getName":
+      return {
+        ...state,
+        fullNames: action.fullName,
+        firstName: "",
+        lastName: ""
+      };
     default:
       throw new Error();
   }
